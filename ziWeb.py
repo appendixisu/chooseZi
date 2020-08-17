1783b9fe6ea45699028f8e2698f1ff0517f9688d
Andy <andy10189911@hotmail.com>
2020-08-17 22:21:19 +0800

    single selection and multiple selection

--- ziWeb.py
+++ ziWeb.py
@@ -13,21 +13,29 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 app.config["JSON_AS_ASCII"] = False
 
+defaultPath = '/Users/wei-chilan/Documents/python/chooseZi'
+# defaultPath = '/home/tooxen_llc/web'
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
+@app.route('/signal', methods=['GET'])
+def singal():
+    return render_template('signal.html')
 
 @app.route('/number', methods=['GET'])
 def number():
-    path = '/Users/wei-chilan/Documents/python/chooseZi/imgs'
+    numOfitems = request.args.get('numOfitems')
+    x = numOfitems if numOfitems is not None else 10
+
+    path = defaultPath+'/imgs'
     list = os.listdir(path)
     if len(list) >= 10:
         random_filename = random.sample([
             x for x in list
             if os.path.isfile(os.path.join(path, x))
-        ], 10)
+        ], int(x))
     else:
         random_filename = [
             x for x in list
@@ -43,7 +51,7 @@ def number():
 
 @app.route('/img/<filename>', methods=['GET'])
 def image(filename):
-    path = '/Users/wei-chilan/Documents/python/chooseZi/imgs'
+    path = defaultPath+'/imgs'
     # random_filename = random.choice([
     #     x for x in os.listdir(path)
     #     if os.path.isfile(os.path.join(path, x))
@@ -59,16 +67,15 @@ def image(filename):
 @app.route('/check/<filename>', methods=['GET'])
 def check(filename):
     isRight = request.args.get('isRight')
-    print(isRight)
 
-    path = '/Users/wei-chilan/Documents/python/chooseZi/imgs'
+    path = defaultPath+'/imgs'
     if isRight == '1':
         SourceFolder = os.path.join(path, filename)
-        TargetFolder = '/Users/wei-chilan/Documents/python/chooseZi/right'
+        TargetFolder = defaultPath+'/right'
         shutil.move(SourceFolder, TargetFolder)
     else:
         SourceFolder = os.path.join(path, filename)
-        TargetFolder = '/Users/wei-chilan/Documents/python/chooseZi/left'
+        TargetFolder = defaultPath+'/left'
         shutil.move(SourceFolder, TargetFolder)
 
     t = {
